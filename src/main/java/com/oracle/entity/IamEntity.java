f11c4c9eb763b2849dfe8838bf77dbf16bf58f4e
shell <451121279@qq.com>
2018-03-07 10:36:33 +0800

    aa

---
+++ src/main/java/com/oracle/entity/IamEntity.java
@@ -0,0 +1,27 @@
+package com.oracle.entity;
+
+public class IamEntity {
+   private String name;
+   private int age;
+   private int num;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	   
+   
+}
